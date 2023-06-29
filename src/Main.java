bf0652f0ed58406027b3a8433f34ba9e87057eb4
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-06-29 12:16:05 +0500

    f

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> intListResult = new ArrayList<>();
+        for (int v : intList) {
+            if (v > 0 && v % 2 == 0) intListResult.add(v);
+        }
+        for (int val : intListResult) {
+            System.out.println(val);
+        }
+    }
+}
